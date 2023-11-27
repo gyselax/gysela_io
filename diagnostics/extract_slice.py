0e2353273bc45ca3cc34ea0aa9988f129158af15
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2023-11-27 14:51:10 +0100

    Add the forgotten folder diagnostics/

---
+++ diagnostics/extract_slice.py
@@ -0,0 +1,105 @@
+# SPDX-License-Identifier: MIT
+
+"""
+Created on 2023-11-23
+
+ This python script initialise the distribution function that
+ will be read by GYSELA
+
+@author: P. Donnel & V. Grandgirard
+"""
+
+import h5py
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import time
+import xarray as xr
+import yaml
+
+from argparse import ArgumentParser
+from pathlib import Path
+from yaml.loader import SafeLoader
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description="Extract slice from 5D distribution function")
+    parser.add_argument('-i','--input_file',
+                        action='store',
+                        nargs='?',
+                        default=Path('GyselaX_fdistribu_rst00000.nc'),
+                        type=Path,
+                        required=False,
+                        help='NetCDF or HDF5 file containing the distribution function')
+    parser.add_argument('--keys', action='store_true',help='List the dataset contained in the input file')
+    parser.add_argument('-d','--dataset',
+                        action='store',
+                        nargs='?',
+                        default='fdistribu',
+                        type=str,
+                        help='Name of the dataset to plot')
+    parser.add_argument('-s','--select',
+                        action='store',
+                        nargs='?',
+                        default='tor1=0,tor2=0,tor3=0,vpar=:,mu=:,species=0',
+                        type=str,
+                        help="Position of the slice to extract, by default 'tor1=0,tor2=0,tor3=0,vpar=:,mu=:,species=0'")
+    args = parser.parse_args()
+    print(args)
+
+    input_file = args.input_file
+    dataset_name = args.dataset
+    str_select = args.select.replace(" ","")
+
+    if args.keys:
+      os.system('h5ls {}'.format(input_file))
+      sys.exit()
+
+    if input_file.suffix == '.nc':
+      ds_gysela = xr.open_dataset(input_file)
+    elif input_file.suffix == '.h5':
+      with h5py.File(input_file, "r") as h5f:
+        species = h5f['species'][()]
+        for i in range(0,len(species)):
+          species[i]=species[i].decode('utf-8')
+        ds_gysela = xr.Dataset(
+            data_vars=dict(
+            densityTorCS=(['tor1','tor2'], h5f['densityTorCS'][()]),
+            UparTorCS=(['tor1','tor2'], h5f['UparTorCS'][()]),
+            temperatureTorCS=(['tor1','tor2'], h5f['temperatureTorCS'][()]),
+            fdistribu=(['tor1', 'tor2', 'tor3', 'vpar', 'mu', 'species'], h5f['fdistribu'][()] )
+            ),
+            coords=dict(
+              tor1=h5f['grid_tor1'][()],
+              tor2=h5f['grid_tor2'][()],
+              tor3=h5f['grid_tor3'][()],
+              vpar=h5f['grid_vpar'][()],
+              mu=h5f['grid_mu'][()],
+              species=species,
+            ),
+            attrs=dict(description="Mesh and initial profiles of GyselaX"),
+        )
+    
+    list_select_OK = []
+    str_select_OK = ''
+    for i in str_select.split(','):
+      if i.split('=')[0] in ds_gysela[dataset_name].coords:
+        if ':' not in i:
+          list_select_OK.append(i)
+          str_select_OK = str_select_OK+'_i'+str(i).replace('=','eq')
+        else:
+          str_select_OK = str_select_OK+'_'+str(i).replace('=:','')
+
+    dict_select = {key: int(value) for key, value in (pair.split('=') for pair in list_select_OK)}
+    print(dict_select)
+    ds_select = ds_gysela[dataset_name].isel(dict_select)
+    print(ds_select)
+
+    if len(ds_select.dims)>0 :
+      ax = plt.axes()
+      ds_select.plot()
+      figname = '{}{}.png'.format(dataset_name,str_select_OK)
+      print('--> Figure saved in: {}'.format(figname))
+      plt.savefig(figname)
+    else:
+      print(ds_select) 
